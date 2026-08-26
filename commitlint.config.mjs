4c3fd7a0d5f1a753311e3b1a89748c265681bd7c
Eleno <elenoxe@gmail.com>
2026-08-26 22:09:46 +0800

    chore(tooling): enforce commit message conventions

---
+++ commitlint.config.mjs
@@ -0,0 +1,25 @@
+const TYPES = ["feat", "fix", "refactor", "test", "chore", "docs"]
+const SCOPES = ["client", "server", "fullstack", "tooling"]
+
+export default {
+  extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "scope-required": ({ type, scope }) => [
+          type === "docs" || Boolean(scope),
+          "scope is required except for docs commits",
+        ],
+      },
+    },
+  ],
+  rules: {
+    "type-enum": [2, "always", TYPES],
+    "scope-enum": [2, "always", SCOPES],
+    "scope-required": [2, "always"],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "header-max-length": [2, "always", 100],
+    "body-leading-blank": [2, "always"],
+  },
+}
