803273e51c16539e5f72870bf8a9f3b4deeb9c86
Eleno <elenoxe@gmail.com>
2026-09-10 23:12:03 +0800

    refactor(server): expose public task interfaces from package root

--- apps/server/src/riva/tasks/__init__.py
+++ apps/server/src/riva/tasks/__init__.py
@@ -1,13 +1,26 @@
+from riva.tasks.core.app import app
 from riva.tasks.core.context import TaskResources, get_task_resources
-from riva.tasks.core.dispatch import cancel_job, defer_job
+from riva.tasks.core.dispatch import cancel_job, defer_job, get_job_status, retry_job
+from riva.tasks.core.schema import reset_task_schema, setup_task_schema
+from riva.tasks.core.worker import run_worker
 from riva.tasks.errors import TaskError
 from riva.tasks.registry import Task
+from riva.tasks.types import JobStatus, TaskState, TaskStatus
 
 __all__ = [
+    "JobStatus",
     "Task",
     "TaskError",
     "TaskResources",
+    "TaskState",
+    "TaskStatus",
+    "app",
     "cancel_job",
     "defer_job",
+    "get_job_status",
     "get_task_resources",
+    "reset_task_schema",
+    "retry_job",
+    "run_worker",
+    "setup_task_schema",
 ]
